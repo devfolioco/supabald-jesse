3358fee0e8c2463283de1013e3fb9c94ba3b11fd
Preet Parekh <preetjdpdev@gmail.com>
2024-05-28 09:53:48 +0530

    🚧 Add nominate route
    🚧 Implement user search
    
    🚧 Implement state
    
    🚧 Update copy
    
    🚧 Update navigation

--- src/app/frog/index.ts
+++ src/app/frog/index.ts
@@ -1,5 +1,5 @@
 import { createSystem } from 'frog/ui'
- 
+
 export const {
   Box,
   Columns,
--- src/app/utils/shared.ts
+++ src/app/utils/shared.ts
@@ -1,3 +1,9 @@
 export const APP_URL = `https://${process.env.VERCEL_URL}` || 'http://localhost:3003';
 
 export const OPENSEA_COLLECTION = 'https://opensea.io/collection/supabald-jesse';
+
+export function isNumeric(str: string | number) {
+    if (typeof str != "string") return false // we only process strings!  
+    return !isNaN(str as unknown as number) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+        !isNaN(parseFloat(str)) // ...and ensure strings of whitespace fail
+}
