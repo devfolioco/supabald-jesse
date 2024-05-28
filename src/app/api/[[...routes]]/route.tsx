2ce3b4e9fd03e98d1f3dad31083b6683b5e177f1
Preet Parekh <preetjdpdev@gmail.com>
2024-05-29 00:50:47 +0530

    🚧 Add emoji again

--- src/app/api/[[...routes]]/route.tsx
+++ src/app/api/[[...routes]]/route.tsx
@@ -189,6 +189,12 @@ app.frame('/confirm', async c => {
           <Heading size={'32'} weight="500" font={'nyght'}>
             Preview Cast
           </Heading>
+          <Text>gm @{searchUser.username}. @{interactor.username} thinks {"you're"} a super based builder, and has nominated you for the Onchain Summer Buildathon.
+
+            Hop in, mint your SupaBald Jesse NFT, and just build it. LFG
+
+            https://letsgetjessebald.com/`;
+          </Text>
         </VStack>
       </Box>
     ),
