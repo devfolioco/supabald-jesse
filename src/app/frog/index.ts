a93dffae00df70a8721c5a14b13d87ac5b09ae1e
Pratham Vaidya <iamprathamvaidya@gmail.com>
2024-05-28 21:48:05 +0530

    updated: frame designs and added required assets

--- src/app/frog/index.ts
+++ src/app/frog/index.ts
@@ -1,59 +1,50 @@
-import fs from "fs"
+import fs from 'fs';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-import { createSystem, defaultVars } from 'frog/ui'
+import { createSystem, defaultVars } from 'frog/ui';
 
-const nyghtSerifMedium = fs.readFileSync(path.join(fileURLToPath(import.meta.url), '../../fonts/NyghtSerif-Medium.ttf'))
-const nyghtSerifBold = fs.readFileSync(path.join(fileURLToPath(import.meta.url), '../../fonts/NyghtSerif-Bold.ttf'))
+const nyghtSerifMedium = fs.readFileSync(
+  path.join(fileURLToPath(import.meta.url), '../../fonts/NyghtSerif-Medium.ttf')
+);
+const nyghtSerifBold = fs.readFileSync(path.join(fileURLToPath(import.meta.url), '../../fonts/NyghtSerif-Bold.ttf'));
 
-export const {
-  Box,
-  Columns,
-  Column,
-  Divider,
-  Heading,
-  HStack,
-  Icon,
-  Image,
-  Rows,
-  Row,
-  Spacer,
-  Text,
-  VStack,
-  vars,
-} = createSystem({
-  colors: {
-    ...defaultVars.colors,
-    text: '#F3F3F3',
-    background: '#638596',
-  },
-  fonts: {
-    default: [
-      {
-        name: 'Inter',
-        source: 'google',
-        weight: 300,
-      },
-      {
-        name: 'Inter',
-        source: 'google',
-        weight: 600,
-      },
-    ],
-    nyght: [
-      {
-        name: 'NyghtSerif-Medium',
-        data: nyghtSerifMedium,
-        source: 'buffer',
-        weight: 400
-      },
-      {
-        name: 'NyghtSerif-Bold',
-        data: nyghtSerifBold,
-        source: 'buffer',
-        weight: 700
-      }
-    ]
-  }
-})
+export const { Box, Columns, Column, Divider, Heading, HStack, Icon, Image, Rows, Row, Spacer, Text, VStack, vars } =
+  createSystem({
+    colors: {
+      ...defaultVars.colors,
+      text: '#F3F3F3',
+      background: '#638596',
+      castBackground: '#1B1423',
+      castColor: '#C848FF',
+      castGrey: '#8B99A4',
+    },
+    fonts: {
+      default: [
+        {
+          name: 'Inter',
+          source: 'google',
+          weight: 300,
+        },
+        {
+          name: 'Inter',
+          source: 'google',
+          weight: 600,
+        },
+      ],
+      nyght: [
+        {
+          name: 'NyghtSerif-Medium',
+          data: nyghtSerifMedium,
+          source: 'buffer',
+          weight: 400,
+        },
+        {
+          name: 'NyghtSerif-Bold',
+          data: nyghtSerifBold,
+          source: 'buffer',
+          weight: 700,
+        },
+      ],
+    },
+  });
