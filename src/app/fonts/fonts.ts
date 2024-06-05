69efeec2919303431df13e059031dbd9cb94224f
Pratham Vaidya <iamprathamvaidya@gmail.com>
2024-06-05 09:35:35 +0530

    updated: used ttf over woff2 fonts deleted: unused woff2 fonts

--- src/app/fonts/fonts.ts
+++ src/app/fonts/fonts.ts
@@ -2,17 +2,17 @@ import localFont from 'next/font/local';
 import { Inter } from 'next/font/google';
 
 export const nyghtRegular = localFont({
-  src: '../fonts/NyghtSerif-Regular.woff2',
+  src: '../fonts/NyghtSerif-Regular.ttf',
   display: 'swap',
 });
 
 export const nyghtBold = localFont({
-  src: '../fonts/NyghtSerif-Bold.woff2',
+  src: '../fonts/NyghtSerif-Bold.ttf',
   display: 'swap',
 });
 
 export const nyghtMedium = localFont({
-  src: '../fonts/NyghtSerif-Medium.woff2',
+  src: '../fonts/NyghtSerif-Medium.ttf',
   display: 'swap',
 });
 
