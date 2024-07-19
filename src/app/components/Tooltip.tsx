f79fc614dbfae364707ea74b4322cf57e6b9d817
Pratham Vaidya <iamprathamvaidya@gmail.com>
2024-07-19 12:14:38 +0530

    🐛 Fixed gap on higher resolution

--- src/app/components/Tooltip.tsx
+++ src/app/components/Tooltip.tsx
@@ -10,9 +10,9 @@ const Tooltip = ({
   return (
     <span className={`inline-flex group relative max-w-max flex-col items-center justify-center ${className}`}>
       <span className="underline cursor-pointer">{children}</span>
-      <span className="absolute left-1/2 top-10 ml-auto mr-auto min-w-max -translate-x-1/2 opacity-0 transform rounded-lg px-3 py-2 text-xs font-medium transition-opacity duration-300 group-hover:opacity-100 origin-top pointer-events-none">
+      <span className="absolute left-1/2 top-7 ml-auto mr-auto min-w-max -translate-x-1/2 opacity-0 transform rounded-lg px-3 py-2 text-xs font-medium transition-opacity duration-300 group-hover:opacity-100 origin-top pointer-events-none">
         <span className="flex max-w-xs flex-col items-center shadow-lg">
-          <span className="clip-bottom h-2 w-4 bg-gray-800"></span>
+          <span className="clip-bottom h-2 w-4 bg-gray-800 mb-[-1px]"></span>
           <span className="rounded-2xl bg-[#273339] p-4 text-center text-xs text-white">{message}</span>
         </span>
       </span>
